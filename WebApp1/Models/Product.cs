1d70bc261229ab60240ce9370edcb560bab3a4a5
sumalatha2005 <109894734+sumalatha2005@users.noreply.github.com>
2022-09-26 16:08:00 +0530

    build suss

--- WebApp1/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp1.Models
-{
-    public class Product
-    {
-         public int ProductId { get; set; }
-         public string ProductName { get; set; }
-         public int Quantity { get; set; }
-    }
-}
